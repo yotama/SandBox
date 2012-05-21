119f9384b6c2f65e01d84f6a8208e36d2a08f0bf
Yotama <yotayotama@gmail.com>
2012-05-22 03:53:06 +0900

    Initial commit

---
+++ SandBox/src/main/java/com/yotagumi/SandBox/PHPSerializer.java
@@ -0,0 +1,171 @@
+package com.yotagumi.SandBox;
+
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.beanutils.PropertyUtils;
+
+/**
+ * PHPSerializer
+ * 
+ * @author Yotama
+ */
+public class PHPSerializer {
+	/**
+	 * Serialize to PHP format.
+	 * 
+	 * @param object
+	 * @return PHP serialized string
+	 */
+	public String serialize(Object object) {
+		StringBuilder result = new StringBuilder();
+		doSerialize(object, result);
+
+		return result.toString();
+	}
+
+	/**
+	 * Core method of serialize.
+	 * 
+	 * @param object
+	 * @param result
+	 */
+	private void doSerialize(Object object, StringBuilder result) {
+		if (object == null) {
+			result.append("N;");
+		} else if (object instanceof String) {
+			serializeString((String) object, result);
+		} else if (object instanceof Boolean) {
+			serializeBoolean((Boolean) object, result);
+		} else if (object instanceof Integer || object instanceof Long
+				|| object instanceof Byte || object instanceof Short) {
+			serializeInteger((Number) object, result);
+		} else if (object instanceof Number) {
+			serializeDouble((Number) object, result);
+		} else if (object instanceof Map) {
+			serializeMap((Map<?, ?>) object, result);
+		} else if (object instanceof List) {
+			serializeList((List<?>) object, result);
+		} else if (object instanceof Object) {
+			serializeBean(object, result);
+		}
+	}
+
+	/**
+	 * Serialize string
+	 * 
+	 * @param str
+	 *            target string
+	 * @param result
+	 *            output buffer for PHP format
+	 */
+	private void serializeString(String str, StringBuilder result) {
+		result.append("s:").append(str.length()).append(":\"").append(str)
+				.append("\";");
+	}
+
+	/**
+	 * Serialize boolean
+	 * 
+	 * @param bool
+	 * @param result
+	 */
+	private void serializeBoolean(Boolean bool, StringBuilder result) {
+		result.append("b:").append(bool ? "1" : "0").append(";");
+	}
+
+	/**
+	 * Serialize integer
+	 * 
+	 * @param number
+	 * @param result
+	 */
+	private void serializeInteger(Number number, StringBuilder result) {
+		result.append(String.format("i:%s;", number.toString()));
+	}
+
+	/**
+	 * Serialize double
+	 * 
+	 * @param number
+	 * @param result
+	 */
+	private void serializeDouble(Number number, StringBuilder result) {
+		result.append("d:").append(number.toString()).append(";");
+	}
+
+	/**
+	 * Serialize Map
+	 * 
+	 * @param number
+	 * @param result
+	 */
+	private void serializeMap(Map<?, ?> map, StringBuilder result) {
+		result.append("a:").append(map.size()).append(":{");
+		for (Object item : map.keySet()) {
+			doSerialize(item, result);
+			doSerialize(map.get(item), result);
+		}
+		result.append("}");
+	}
+
+	/**
+	 * Serialize List
+	 * 
+	 * @param list
+	 * @param result
+	 */
+	private void serializeList(List<?> list, StringBuilder result) {
+		result.append("a:").append(list.size()).append(":{");
+		int i = 0;
+		for (Object item : list) {
+			doSerialize(i++, result);
+			doSerialize(item, result);
+		}
+		result.append("}");
+	}
+
+	/**
+	 * Serialize Bean
+	 * 
+	 * @param object
+	 * @param result
+	 */
+	private void serializeBean(Object object, StringBuilder result) {
+		result.append("a:");
+
+		PropertyDescriptor[] descriptors = PropertyUtils.getPropertyDescriptors(object.getClass());
+
+		List<PropertyDescriptor> targetDescriptor = new ArrayList<>();
+		int propertyCount = 0;
+		for (PropertyDescriptor descriptor : descriptors) {
+			Method readMethod = descriptor.getReadMethod();
+			if (readMethod.getDeclaringClass() == Object.class) {
+				continue;
+			}
+			propertyCount++;
+			targetDescriptor.add(descriptor);
+		}
+
+		result.append(propertyCount).append(":{");
+
+		for (PropertyDescriptor descriptor : targetDescriptor) {
+			Method readMethod = descriptor.getReadMethod();
+			Object property = null;
+			try {
+				property = readMethod.invoke(object, new Object[] {});
+			} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+				e.printStackTrace();
+			}
+			String propertyName = descriptor.getName();
+			result.append("s:").append(propertyName.length()).append(":\"").append(propertyName).append("\";");
+			doSerialize(property, result);
+		}
+		
+		result.append("}");
+	}
+}
---
+++ SandBox/src/test/java/com/yotagumi/SandBox/PHPSerializerTest.java
@@ -0,0 +1,100 @@
+package com.yotagumi.SandBox;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Test for PHPSerialize
+ * 
+ * @author Yotama
+ * 
+ */
+public class PHPSerializerTest {
+	PHPSerializer target;
+
+	@Before
+	public void setUp() throws Exception {
+		target = new PHPSerializer();
+	}
+
+	@Test
+	public void test_serialize_String() {
+		assertThat(target.serialize("Yotama"), is("s:6:\"Yotama\";"));
+	}
+
+	@Test
+	public void test_serialize_Boolean() {
+		assertThat(target.serialize(Boolean.valueOf("true")), is("b:1;"));
+		assertThat(target.serialize(Boolean.valueOf("false")), is("b:0;"));
+	}
+	
+	@Test
+	public void test_serialize_Integer() {
+		assertThat(target.serialize(Integer.valueOf(Integer.MAX_VALUE)), is("i:2147483647;"));
+		assertThat(target.serialize(Long.valueOf(Long.MAX_VALUE)), is("i:9223372036854775807;"));
+		assertThat(target.serialize(Byte.valueOf(Byte.MAX_VALUE)), is("i:127;"));
+		assertThat(target.serialize(Short.valueOf(Short.MAX_VALUE)), is("i:32767;"));
+	}
+	
+	@Test
+	public void test_serialized_Double() {
+		assertThat(target.serialize(Double.valueOf(0.01d)), is("d:0.01;"));
+		assertThat(target.serialize(Float.valueOf(0.1f)), is("d:0.1;"));
+	}
+
+	@Test
+	public void test_serialized_Map() {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("name", "Yotama");
+		map.put("age", 28);
+		assertThat(target.serialize(map), is("a:2:{s:3:\"age\";i:28;s:4:\"name\";s:6:\"Yotama\";}"));
+	}
+	
+	@Test
+	public void test_serialized_List() {
+		List<String> list = new ArrayList<String>();
+		list.add("Kagoshima");
+		list.add("Tokyo");
+		assertThat(target.serialize(list), is("a:2:{i:0;s:9:\"Kagoshima\";i:1;s:5:\"Tokyo\";}"));
+	}
+
+	@Test
+	public void test_serialize_bean() {
+		class TestBaen {
+			private String name;
+			private int age;
+
+			@SuppressWarnings("unused")
+			public String getName() {
+				return name;
+			}
+
+			public void setName(String name) {
+				this.name = name;
+			}
+
+			@SuppressWarnings("unused")
+			public int getAge() {
+				return age;
+			}
+
+			public void setAge(int age) {
+				this.age = age;
+			}
+		}
+
+		TestBaen dto = new TestBaen();
+		dto.setName("Yotama");
+		dto.setAge(28);
+
+		assertThat(target.serialize(dto), is("a:2:{s:3:\"age\";i:28;s:4:\"name\";s:6:\"Yotama\";}"));
+	}
+}
